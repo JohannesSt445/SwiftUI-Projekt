3e1b93f3c0c39e51f2e3555229b17c2f374449c2
Johannes Stümer / BBS2H20A <Johannes.Stuemer@edu.bib.de>
2022-01-25 08:18:05 +0100

    Initial commit(anlegen des Projekts)

---
+++ SwiftUIApp-Projekt/SwiftUIApp-Projekt/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIApp-Projekt
+//
+//  Created by Johannes Stümer / BBS2H20A on 25.01.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIApp-Projekt/SwiftUIApp-Projekt/SwiftUIApp_ProjektApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApp_ProjektApp.swift
+//  SwiftUIApp-Projekt
+//
+//  Created by Johannes Stümer / BBS2H20A on 25.01.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApp_ProjektApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
